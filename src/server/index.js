d9ad43cb3e8d51835cd5c2fbc5255dd70b46854f
Riza Fahmi <rizafahmi@gmail.com>
2018-03-12 11:34:21 +0700

    Testing hello world

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,17 @@
-console.log('It works!');
+const Koa = require("koa");
+
+const app = new Koa();
+const PORT = 1337;
+
+app.use(async ctx => {
+  ctx.body = {
+    status: "OK",
+    message: "Hello, KOA!"
+  };
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
+
+module.exports = server;
